0ec4b01526ef8fdb17e9b1c51c6e29c4dc22cf52
Noah <45659684+NoahDrucker@users.noreply.github.com>
2019-06-13 15:55:26 +0300

    Create delete_subuser.java

---
+++ java/delete_subuser.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.stream.Collectors;
+
+public class delete_subuser {
+
+	public static void main(String[] args) throws IOException {
+		// Details retrieved from Authentication part.
+		String userId = "";
+		String token = "";
+		int subuserId = 0; // Received from Get Subusers part.
+		
+		URL url = new URL("https://api.smartproxy.com/v1/users/"+userId+"/sub-users/"+subuserId);
+				
+		URLConnection connection = url.openConnection();
+		HttpURLConnection httpConn = (HttpURLConnection) connection;
+		
+		
+		httpConn.setRequestProperty("User-Agent", "Mozilla/5.0");
+		httpConn.setRequestProperty ("Authorization", "Token "+token);
+		httpConn.setRequestMethod("DELETE");
+
+		
+		if (200 <= httpConn.getResponseCode() && httpConn.getResponseCode() <= 299) {
+		    BufferedReader br = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+			System.out.print(br.lines().collect(Collectors.joining()));
+		} else {
+		    BufferedReader br = new BufferedReader(new InputStreamReader(httpConn.getErrorStream()));
+			System.out.print(br.lines().collect(Collectors.joining()));
+		}
+		
+		httpConn.disconnect();
+		
+	}
+
+}
