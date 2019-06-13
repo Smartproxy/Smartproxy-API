f925d874b3e9bb8d2dd3fb18caf026dfbfa95b5c
Noah <45659684+NoahDrucker@users.noreply.github.com>
2019-06-13 15:33:50 +0300

    Create delete_whitelisted_ip.php

---
+++ php/delete_whitelisted_ip.php
@@ -0,0 +1,26 @@
+<?php
+
+// Information obtained from authentication
+
+$userId = '';
+$token = '';
+$id = ''; // id of your whitelisted ip, you can acquire them by requesting your IP list through API.
+$url = 'https://api.smartproxy.com/v1/users/' . $userId . '/whitelisted-ips/' . $id;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+
+$headers = array();
+$headers[] = "Accept: application/json";
+$headers[] = "Authorization: Token $token";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+echo $result;
