9ad8396751880df58c6c67c1a428a526f41e77de
Noah <45659684+NoahDrucker@users.noreply.github.com>
2019-06-13 15:49:41 +0300

    Create update_subuser.java

---
+++ java/update_subuser.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.stream.Collectors;
+
+public class update_subuser {
+	
+	public static void main(String[] args) throws IOException {
+		// Details retrieved from Authentication part.
+		String userId = "";
+		String token = "";
+		int subuserId = 0; // Retrieved from "Get Subusers"
+		
+		// Details of the subuser you wish to change
+		String payload = "{\"password\":\"new_password\",\"traffic_limit\":0}";
+		
+		URL url = new URL("https://api.smartproxy.com/v1/users/"+userId+"/sub-users/"+subuserId);
+				
+		URLConnection connection = url.openConnection();
+		HttpURLConnection httpConn = (HttpURLConnection) connection;
+		
+		
+		httpConn.setRequestProperty("User-Agent", "Mozilla/5.0");
+		httpConn.setRequestProperty ("Authorization", "Token "+token);
+		httpConn.setRequestMethod("PUT");
+		httpConn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+		httpConn.setRequestProperty("Accept", "application/json");
+
+		httpConn.setDoOutput(true);
+		OutputStream outStream = httpConn.getOutputStream();
+		OutputStreamWriter outStreamWriter = new OutputStreamWriter(outStream, "UTF-8");
+		outStreamWriter.write(payload);
+		outStreamWriter.flush();
+		outStreamWriter.close();
+		outStream.close();
+		
+		if (200 <= httpConn.getResponseCode() && httpConn.getResponseCode() <= 299) {
+		    BufferedReader br = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+			System.out.print(br.lines().collect(Collectors.joining()));
+		} else {
+		    BufferedReader br = new BufferedReader(new InputStreamReader(httpConn.getErrorStream()));
+			System.out.print(br.lines().collect(Collectors.joining()));
+		}
+		
+		httpConn.disconnect();
+		
+	}
+
+}
