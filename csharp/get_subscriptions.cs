a832443dcd3467db8e581af5a26a787ee9ccfe3d
Noah <45659684+NoahDrucker@users.noreply.github.com>
2019-06-19 17:43:46 +0300

    Create get_subscriptions.cs

---
+++ csharp/get_subscriptions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Net;
+
+namespace Smartproxy_API
+{
+    class get_subscriptions
+    {
+        static void Main(string[] args)
+        {
+            // Details from authentication
+            string userId = "";
+            string token = "";
+
+            string uri = "https://api.smartproxy.com/v1/users/" + userId + "/subscriptions";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.PreAuthenticate = true;
+            request.Headers.Add("Authorization", "Token " + token);
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+                Console.ReadKey();
+            }
+        }
+    }
+}
