4642da2b0c7e880f6256f9d6b33ed96f8e53e71e
Noah <45659684+NoahDrucker@users.noreply.github.com>
2019-06-19 17:43:01 +0300

    Create delete_whitelisted_ips.cs

---
+++ csharp/delete_whitelisted_ips.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Smartproxy_API
+{
+    class delete_whitelisted_ips
+    {
+        static void Main(string[] args)
+        {
+            // Details from authentication
+            string userId = "";
+            string token = "";
+
+            int id = 73342; // Recieved from get_whitelisted_ips.cs
+
+            string uri = "https://api.smartproxy.com/v1/users/" + userId + "/whitelisted-ips/" + id;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.PreAuthenticate = true;
+            request.Headers.Add("Authorization", "Token " + token);
+            request.ContentType = "application/json";
+            request.Method = "DELETE";
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+                Console.ReadKey();
+            }
+        }
+    }
+}
